1675c31d221ebbf74a261b8be32d456995ebc2e6
Shahryar <panjwanishahryar@yahoo.com>
2024-05-25 11:06:34 -0400

    updated

---
+++ app/src/main/java/ssalim/example/capstone_balllife/MainActivity.kt
@@ -0,0 +1,11 @@
+package ssalim.example.capstone_balllife
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
