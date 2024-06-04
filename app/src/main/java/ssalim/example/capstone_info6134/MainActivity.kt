eb78794166577a476197845910662708e475c5e8
Mufeed Muneer <m_muneer@fanhsaweonline.ca>
2024-06-04 16:35:31 -0400

    test commit

--- app/src/main/java/ssalim/example/capstone_info6134/MainActivity.kt
+++ app/src/main/java/ssalim/example/capstone_info6134/MainActivity.kt
@@ -2,10 +2,13 @@ package ssalim.example.capstone_info6134
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mufee:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
