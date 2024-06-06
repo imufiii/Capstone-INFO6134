feff78d6c5e6c8648475eef60b033b2741d75b53
Shahryar <panjwanishahryar@yahoo.com>
2024-06-06 12:51:57 -0400

    changed activity

--- app/src/main/java/ssalim/example/capstone_info6134/MainActivity.kt
+++ app/src/main/java/ssalim/example/capstone_info6134/MainActivity.kt
@@ -14,7 +14,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu, menu)
         return true
     }
@@ -29,7 +28,6 @@ class MainActivity : AppCompatActivity() {
 
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here.
         return when (item.itemId) {
             R.id.action_settings -> {
                 Toast.makeText(this, "Settings selected", Toast.LENGTH_SHORT).show()
