920f487c2a4c837f2d6ea0b7d3317b738078f881
Shahryar <panjwanishahryar@yahoo.com>
2024-06-08 23:17:55 -0400

    updated logos and made layout of main file

--- app/src/main/java/ssalim/example/capstone_info6134/MainActivity.kt
+++ app/src/main/java/ssalim/example/capstone_info6134/MainActivity.kt
@@ -2,10 +2,42 @@ package ssalim.example.capstone_info6134
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    fun onCardClick(view: View) {
+        when (view.id) {
+            R.id.cardView -> Toast.makeText(this, "Card 1 Clicked", Toast.LENGTH_SHORT).show()
+            R.id.cardView2 -> Toast.makeText(this, "Card 2 Clicked", Toast.LENGTH_SHORT).show()
+            R.id.cardView3 -> Toast.makeText(this, "Card 3 Clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                Toast.makeText(this, "Settings selected", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_about -> {
+                Toast.makeText(this, "About selected", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
