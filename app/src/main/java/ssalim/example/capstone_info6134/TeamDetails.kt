361c25b8b3aa73d2ef6f24f4a882b0975417e5d3
Mufeed Muneer <117446007+imufiii@users.noreply.github.com>
2024-06-09 00:01:25 -0400

    test commit

--- app/src/main/java/ssalim/example/capstone_info6134/TeamDetails.kt
+++ app/src/main/java/ssalim/example/capstone_info6134/TeamDetails.kt
@@ -5,10 +5,7 @@ import android.os.Bundle
 
 class TeamDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/main
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_team_details)
     }
