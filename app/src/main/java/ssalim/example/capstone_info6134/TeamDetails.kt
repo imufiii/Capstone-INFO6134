40459d64df33d69f4f91aa57cada90dbab9b6ec8
Shahryar <panjwanishahryar@yahoo.com>
2024-06-08 20:28:35 -0400

    added logos and headings

---
+++ app/src/main/java/ssalim/example/capstone_info6134/TeamDetails.kt
@@ -0,0 +1,11 @@
+package ssalim.example.capstone_info6134
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TeamDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_team_details)
+    }
+}
