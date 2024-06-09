9cb283ed0719bdbd99859654e7229d433ba6ad4c
Mufeed Muneer <117446007+imufiii@users.noreply.github.com>
2024-06-09 16:45:56 -0400

    #9 i created a separate shared preference object and function to save their favourite team

--- app/src/main/java/ssalim/example/capstone_info6134/sharedPreference.kt
+++ app/src/main/java/ssalim/example/capstone_info6134/sharedPreference.kt
@@ -4,5 +4,21 @@ import android.content.Context
 import android.content.SharedPreferences
 
 object sharedPreference {
-//
+
+    private const val PREF_NAME = "app_prefs"
+    private const val KEY_FAVORITE_TEAM = "favorite_team"
+
+
+    fun saveTeam(context: Context, team: String) {
+        val preferences: SharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+        val editor = preferences.edit()
+        editor.putString(KEY_FAVORITE_TEAM, team)
+        editor.apply()
+    }
+
+
+    fun getSavedTeam(context: Context): String? {
+        val preferences: SharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+        return preferences.getString(KEY_FAVORITE_TEAM, null)
+    }
 }
